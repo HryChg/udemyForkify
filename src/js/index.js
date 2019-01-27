a1fbd0bdb29115642db25c7a3988214acfd45f64
harrychuang <vrjgik5@gmail.com>
2019-01-27 00:39:00 -0800

    testing import/export statement and created MVC structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`Just imported ${num} from another module called test.js! Variable x is ${x}`);
+import string from './models/Search';
+// import {add as a, multiply as m, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using Imported Fucntions! ${searchView.add(ID, 2)} and ${searchView.multiply(3, 5)}, ${searchView.ID}`);
