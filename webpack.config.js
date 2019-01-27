fff4ee9452e558472f501bc7633c98209c47731a
harrychuang <vrjgik5@gmail.com>
2019-01-26 23:38:32 -0800

    First commit of udemyForkify

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmpWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmpWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+    
+};
