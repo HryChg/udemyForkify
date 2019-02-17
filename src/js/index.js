f8272273137dd3a7adc489a840635f2ca73c4546
harrychuang <vrjgik5@gmail.com>
2019-02-16 20:07:02 -0800

    cleaned up seach model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 
 const search = new Search('pizza');
-console.log(search);
+// console.log(search);
+
 
 search.getResult();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,28 +5,29 @@ export default class Search {
     }
 
     async getResult() {
+        const proxy = "https://cors-anywhere.herokuapp.com/";
         const food2Fork_key = "f5e0def7610f4c445db7c0c0ed62d483";
-        const edamam_id = "bb08a934";
-        const edamam_key = "109788009c2c1ca6ff5ac36a4698aa0a";
         try {
-            // const res = await axios(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=${edamam_id}&app_key=${edamam_key}`);
-            console.log(result.json());
+            console.log("searching now");
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${food2Fork_key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+
         } catch (err) {
             console.log(err);
         }
     }
 
-    async getInfo(){
+    // EFFECTS: AJAX call to Edamam Food API with keyword: "red apple"
+    async getEdamamResult() {
+        const edamam_id = "bb08a934";
+        const edamam_key = "109788009c2c1ca6ff5ac36a4698aa0a";
+        const ingredient = "red%20apple";
         try {
-            const app_id = "bb08a934";
-            const app_key = "109788009c2c1ca6ff5ac36a4698aa0a";
-            const result = await fetch(`https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=${app_id}&app_key=${app_key}`);
+            const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/food-database/parser?ingr=${ingredient}&app_id=${edamam_id}&app_key=${edamam_key}`);
             console.log(result.json());
-    
         } catch (err) {
             console.log(err);
         }
-        
     }
 }
