daf9281cbdac2c12321b8efebfc9260ce0a1f6c4
harrychuang <vrjgik5@gmail.com>
2019-02-16 20:45:52 -0800

    established a preliminary Search Controller in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,35 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-// console.log(search);
+/** Global state of the app
+* - Search Object
+* - Current recipe object 
+* - Shopping list object
+* - Liked Recipe
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1) get the query from view
+    const query = 'pizza'; // TODO
+
+    if (query) {
+        // 2) New Search Object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+
+    }
+
+}
 
 
-search.getResult();
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -4,30 +4,27 @@ export default class Search {
         this.query = query;
     }
 
-    async getResult() {
+    async getResults() {
         const proxy = "https://cors-anywhere.herokuapp.com/";
         const food2Fork_key = "f5e0def7610f4c445db7c0c0ed62d483";
         try {
-            console.log("searching now");
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${food2Fork_key}&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
-
         } catch (err) {
             console.log(err);
         }
     }
 
     // EFFECTS: AJAX call to Edamam Food API with keyword: "red apple"
-    async getEdamamResult() {
-        const edamam_id = "bb08a934";
-        const edamam_key = "109788009c2c1ca6ff5ac36a4698aa0a";
-        const ingredient = "red%20apple";
-        try {
-            const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/food-database/parser?ingr=${ingredient}&app_id=${edamam_id}&app_key=${edamam_key}`);
-            console.log(result.json());
-        } catch (err) {
-            console.log(err);
-        }
-    }
+    // async getEdamamResult() {
+    //     const edamam_id = "bb08a934";
+    //     const edamam_key = "109788009c2c1ca6ff5ac36a4698aa0a";
+    //     const ingredient = "red%20apple";
+    //     try {
+    //         const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/food-database/parser?ingr=${ingredient}&app_id=${edamam_id}&app_key=${edamam_key}`);
+    //         console.log(result.json());
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // }
 }
